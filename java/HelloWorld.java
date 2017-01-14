bbaf7f86711e8e625b2ea1ca26e7b7fc89a81d95
Naresh Chauhan <Naresh.Chauhan@gmail.com>
2017-01-14 16:46:47 -0500

    hello world project push

---
+++ java/HelloWorld.java
@@ -0,0 +1,15 @@
+
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("hellow world");
+		System.out.println(CreateMessage());
+	}
+	
+	public static String CreateMessage()
+	{
+		return "Hello World";
+	}
+}
