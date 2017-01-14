4f9e5ebf9dd33b22a309e9de85a97f2b153f48f9
naresh-chauhan <naresh.chauhan@gmail.com>
2017-01-14 18:18:33 -0500

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		//System.out.println("hellow world");
 		System.out.println(CreateMessage());
-		test
+
 	}
 	
 	public static String CreateMessage()
