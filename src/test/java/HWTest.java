418852a6863768ea32ea0a3266a30bb80164176e
Naresh Chauhan <Naresh.Chauhan@gmail.com>
2017-01-14 17:03:09 -0500

    hw

---
+++ src/test/java/HWTest.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class HWTest {
+
+	@Test
+	public void testMain() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateMessage() {
+		HelloWorld test = new HelloWorld();
+		String result = test.CreateMessage();
+		assertEquals("Hello Worlds", result);
+			
+		//assertEquals.
+		//fail("Not yet implemented");
+	}
+
+}
