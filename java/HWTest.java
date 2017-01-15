7c653d981c2f4bceec6d6e6d936dcd070d9c1c23
Naresh Chauhan <Naresh.Chauhan@gmail.com>
2017-01-14 22:40:14 -0500

    changed test

---
+++ java/HWTest.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class HWTest {
+
+	@Test
+	public void testMain() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateMessage() {
+		HelloWorld test = new HelloWorld();
+		String result = test.CreateMessage();
+		assertEquals("Hello World!", result);
+			
+		//assertEquals.
+		//fail("Not yet implemented");
+	}
+
+}
